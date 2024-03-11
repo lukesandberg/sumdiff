b759c44e07af9a56c6489b310ccc5bace5a98b7a
Luke Sandberg <lukeisandberg@gmail.com>
2024-03-11 10:30:50 -0700

    Tiny refactoring to main to simplify error handling

--- src/main.rs
+++ src/main.rs
@@ -17,34 +17,30 @@ fn main() {
         eprintln!("Usage: sumdiff <left> <right>: {err}");
         process::exit(1)
     });
-    fn buf_reader(file: &String) -> io::BufReader<fs::File> {
-        io::BufReader::new(
-            fs::OpenOptions::new()
-                .read(true)
-                .open(file)
-                .unwrap_or_else(|err| {
-                    eprintln!("Unable to open {file}: {err}");
-                    process::exit(1)
-                }),
-        )
-    }
     let mut line_tokens = Tokens::new();
-    let mut left_reader = buf_reader(&file_pair.left);
-    let left_lines = lex::lex_lines(&mut line_tokens, &mut left_reader).unwrap_or_else(|err| {
-        eprintln!("Unable to read {0}: {err}", file_pair.left);
-        process::exit(1)
-    });
-    let mut right_reader = buf_reader(&file_pair.right);
-    let right_lines = lex::lex_lines(&mut line_tokens, &mut right_reader).unwrap_or_else(|err| {
-        eprintln!("Unable to read {0}: {err}", file_pair.right);
-        process::exit(1)
-    });
+    let left_lines = tokenize_or_die(&file_pair.left, &mut line_tokens);
+    let right_lines = tokenize_or_die(&file_pair.right, &mut line_tokens);
     let matches = kc::kc_lcs(&line_tokens, &left_lines.tokens, &right_lines.tokens);
     display::display_diff(&line_tokens.printer(), &left_lines, &right_lines, &matches)
         .unwrap_or_else(|err| {
             eprintln!("Error while displaying diff: {err}");
             process::exit(1)
         });
+}
+
+fn tokenize_or_die(file_name: &String, line_tokens: &mut Tokens) -> token::Parsed {
+    let file = fs::OpenOptions::new()
+        .read(true)
+        .open(file_name)
+        .unwrap_or_else(|err| {
+            eprintln!("Unable to open {file_name}: {err}");
+            process::exit(1)
+        });
+    let mut reader = io::BufReader::new(file);
+    lex::lex_lines(line_tokens, &mut reader).unwrap_or_else(|err| {
+        eprintln!("Unable to read {file_name}: {err}");
+        process::exit(1)
+    })
 }
 
 fn parse_args(mut args: impl Iterator<Item = String>) -> Result<FilePair, &'static str> {
